92de443d495b1705c03d7e689f355e09119b5481
nearKim <garfield@snu.ac.kr>
2018-12-17 09:30:31 +0900

    Add postgres database

--- Instagram_clone/settings.py
+++ Instagram_clone/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'Instagram_clone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tutorial_instagram',
+        'USER': 'postgres',
+        'PASSWORD': '0814',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
